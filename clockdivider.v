f84b90e275af77f5bee0cd5f69c38f73b1f64f04
aj0430 <gloryrulztherainforest@gmail.com>
2026-08-05 00:21:18 -0400

    added oven temp control and clock divider

---
+++ clockdivider.v
@@ -0,0 +1,25 @@
+module clockdivider (
+    input wire clk,
+    input wire rst,
+    output reg pulse
+);
+    reg [25:0] counter;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            counter  <= 26'd0;
+            pulse <= 1'b0;
+        end 
+        else begin
+            if (counter == 26'd49_999_999) begin
+                counter  <= 26'd0;
+                pulse <= 1'b1;
+            end 
+            else begin
+                counter  <= counter + 1'b1;
+                pulse <= 1'b0;
+            end
+        end
+    end
+
+endmodule
---
+++ tempcontroller.v
@@ -0,0 +1,43 @@
+module ovenTemp(
+    input reset_temp, 
+    input oven_on,  
+    input pulse, 
+    input clk, 
+    input [7:0] set_temp, 
+    output reg heater_on, 
+    output reg [8:0] current_temp
+    );
+    
+    reg shutoff;
+
+    always @(posedge clk) begin
+        if (reset_temp) begin
+            current_temp <= 0;
+            heater_on <= 0;
+        end
+        else begin
+            if (pulse) begin
+                if(oven_on) begin
+                    if(current_temp <= set_temp + 2 && shutoff != 1) begin
+                        heater_on <= 1;
+                        current_temp <= current_temp + 2;
+                    end
+                    if(current_temp >= set_temp + 2) begin
+                        shutoff <= 1;
+                        heater_on <= 0;
+                    end
+                    if (current_temp <= set_temp -2) begin
+                        shutoff <= 0;
+                    end
+                    if(heater_on == 0) begin
+                        current_temp <= current_temp - 1;
+                    end
+                end
+            end
+                
+        end
+    end
+
+
+
+endmodule
